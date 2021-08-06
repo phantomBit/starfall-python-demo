b51aa0d7dbe3ea9ff150c15034f6b4ed2c09fe6d
PhantomBit <zanson8@gmail.com>
2021-08-06 17:49:48 -0400

    added schema validation for post request

--- starfall/handlers/widget.py
+++ starfall/handlers/widget.py
@@ -1,20 +1,38 @@
 import starfall.models
 import starfall.database
 
+from http import HTTPStatus
+
 import tornado.web
 import aiosqlite
 import json
 import logging
+from jsonschema import validate
+from jsonschema import ValidationError
 
 
 class WidgetsHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Content-Type", 'application/json')
+        self.set_header("Access-Control-Allow-Origin", "*")
+
+    def data_received(self, chunk=None):
+        if self.request.body:
+            return json.loads(self.request.body)
+
+    def validation_error(self, ex):
+        self.finish(json.dumps({
+            'error': {
+                'code': HTTPStatus.BAD_REQUEST,
+                'message': " ".join(["Invalid Request Payload key '", str(ex.relative_path[0]), "':", ex.message])
+            }
+        }))
 
     async def get(self, key) -> None:
         if not key:
             async with aiosqlite.connect(self.application.settings["database"]) as db:
                 results = await starfall.database.list_all_widgets(db)
-                self.set_header("Content-Type", "application/json")
-                self.set_header("Access-Control-Allow-Origin", "*")
+                self.set_default_headers()
                 self.write(
                     json.dumps(
                         [
@@ -32,8 +50,7 @@ class WidgetsHandler(tornado.web.RequestHandler):
         else:
             async with aiosqlite.connect(self.application.settings["database"]) as db:
                 row = await starfall.database.get_widget(db, id=key)
-                self.set_header("Content-Type", "application/json")
-                self.set_header("Access-Control-Allow-Origin", "*")
+                self.set_default_headers()
                 self.write(
                     json.dumps(
                         {
@@ -45,3 +62,21 @@ class WidgetsHandler(tornado.web.RequestHandler):
                         }
                     )
                 )
+
+    async def post(self, args):
+        try:
+            data = self.data_received()
+            schema = {
+                "type": "object",
+                "properties": {
+                    "number": {"type": "integer", "minimum": 0},
+                    "name": {"type": "string", "minLength": 1, "maxLength": 64},
+                },
+            }
+
+            validate(instance=data, schema=schema)
+
+            self.write('true')
+
+        except ValidationError as ex:
+            self.validation_error(ex)
